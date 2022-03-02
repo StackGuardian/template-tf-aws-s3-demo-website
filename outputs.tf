559f12a56a456b8fd9c82dd1b6967ef2dba4455d
akshat0694 <tandonakshat7@gmail.com>
2022-03-02 11:36:07 +0100

    typo

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "smaple_website_url" {
+output "sample_website_url" {
   description = "The sample website url"
   value       = format("%s%s", "http://", module.s3_bucket.s3_bucket_website_endpoint)
 }
