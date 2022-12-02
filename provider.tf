cca94b570414a67a94be3cd4b647e25f9da4c020
Akshat Tandon <tandonakshat7@gmail.com>
2022-12-02 15:16:32 +0100

    use v3.6.0 for s3 module

--- provider.tf
+++ provider.tf
@@ -1,16 +1,4 @@
 provider "aws" {
   skip_metadata_api_check = true
   region                  = var.bucket_region
-  # assume_role {
-  #   role_arn = "arn:aws:iam::${account_id}:role/${assume_role}"
-  # }
-}
-
-terraform {
-  required_version = ">= 0.12.6, <= 1.1.7"
-
-  required_providers {
-    aws    = ">= 3.0, < 4.0"
-    random = "~> 3"
-  }
 }
