a99e087f229aef756a2cf38832895a6a02659006
Akshat Tandon <tandonakshat7@gmail.com>
2022-12-07 10:52:23 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "sample_website_url" {
   description = "The sample website url"
   value       = format("%s%s", "http://", module.s3_bucket.s3_bucket_website_endpoint)
 }
+
+output "bucket_regional_domain_name" {
+  description = "Bucket regional domain name"
+  value = module.s3_bucket.s3_bucket_bucket_regional_domain_name
+}
