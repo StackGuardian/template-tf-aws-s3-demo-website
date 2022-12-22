94a633f080c3f029b5a8962d3a9d17f3a7509176
Akshat Tandon <tandonakshat7@gmail.com>
2022-12-22 12:05:42 +0400

    assume role

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
 provider "aws" {
   skip_metadata_api_check = true
   region                  = var.bucket_region
+  assume_role {
+    role_arn     = "arn:aws:iam::871606792318:role/sg-demo-admin-prod"
+    session_name = "sg-assumed-role"
+    external_id  = "sg-demo-admin-prod"
+  }
 }
